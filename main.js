5f5e24ae44827805ccf0e9c2c4738dd99150dae8
Ankit Ghodke <ankitghodke24@gmail.com>
2024-02-29 17:06:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Fullstack Developer", "Java Developer","Sql Developer"],
+    typeSpeed: 85,
+    backSpeed: 85,
+    backDelay: 1000,
+    loop:true
+});
